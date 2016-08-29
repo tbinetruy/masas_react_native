db44a2ca791fb2b200a9196915b9a26dc6d053f6
0648596375 <tbinetruy@gmail.com>
2016-08-30 00:56:39 +0200

    got SC profile info object in react native

--- components/SC_wrapper.js
+++ components/SC_wrapper.js
@@ -4,8 +4,11 @@ var { SC_sdk } = NativeModules;
 
 var SC = {
   connect() {
-    SC_sdk.SC_login((error, events) => {
-      console.log(events)
+    SC_sdk.SC_login((error, userID) => {
+      console.log(userID)
+      
+      let SC_ID = "ebc8467d2ae245181b131d34b7273c20"
+      fetch("https://api.soundcloud.com/users/" + userID + "?client_id=" + SC_ID).then((r) => console.log(r.json()))
     });
   }
 };
